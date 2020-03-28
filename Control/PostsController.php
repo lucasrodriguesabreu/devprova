b3f0766c2ba6ec62b7991c1daa265c84699777df
Lucas Rodrigues <lucasrodriguesabreu@hotmail.com>
2020-03-28 08:44:29 -0300

    Subindo Control, implementando inserção, atualizar e deletar

--- Control/PostsController.php
+++ Control/PostsController.php
@@ -9,6 +9,46 @@ class PostsController{
         $response = $response->withJson($lista);
         $response = $response->withHeader("Content-type", "application/json");
         return $response;
+
+    }
+        
+    public function inserir($request, $response, $args){
+        $var = $request->getParsedBody();
+        $post = new Posts(0, $var['description'], $var['completed'], $var['created_at'], $var['update_at']);
+        //$post = new Posts($var['description'], $var['completed'], $var['created_at'], $var['update_at']);
+        $dao = new PostsDAO();
+        $response = $response->withJson($post);
+        $response = $response->withHeader('Content-type', 'application/json');
+        $response = $response->withStatus(201);
+        return $response;
+    }
+        
+    public function atualizar($request, $response, $args){
+        $id = (int) $args['id'];
+        $var = $request->getParsedBody();
+        $post = new Posts($id, $var['description'], $var['completed'], $var['update_at']);
+        $dao = new PostsDAO;
+        $dao->atualizar($posts);
+        $response = $response->withJson($posts);
+        $response = $response->withHeader('Content-type', 'application/json');
+        return $response;
+    }
+    
+    public function deletar(\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Message\ResponseInterface $response, $args){
+        $id = (int) $args['id'];
+        $dao = new PostsDAO();
+        $post = $dao->buscaPorId($id);
+        $dao->deletar($id);
+        return $response->withStatus(200)->withHeader('Location', "http://localhost/deletar");
+    }
+
+    public function buscaPorPost($request, $response, $args){
+        $id = (int) $args['id'];
+        $dao = new PostsDAO();
+        $post = $dao->buscaPorPost();
+        $response = $response->withJson($post);
+        $response = $response->withHeader('Content-type', 'application/json');
+        return $response;
     }
 }
 ?>
